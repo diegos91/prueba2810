2969e44682003eed5edbc792e7dac61d7bbd95a4
Diego <diegorubensanz@gmail.com>
2013-10-28 23:07:29 -0300

    Agregue clase UserDiego

---
+++ src/prueba2810/UserDiego.java
@@ -0,0 +1,24 @@
+package prueba2810;
+
+public class UserDiego {
+
+	private String nombre;
+	private String segundoNombre;
+	private String apellido;
+	
+	public UserDiego() {
+		nombre = null;
+		segundoNombre = null;
+		apellido = null;
+	}
+	
+	public String toString() {
+		return "" + nombre + segundoNombre + apellido;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
